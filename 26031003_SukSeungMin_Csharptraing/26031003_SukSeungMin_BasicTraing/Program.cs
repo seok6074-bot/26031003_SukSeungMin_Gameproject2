7dde795fa1c0ec767bade1630cfc99f065a1bea6
seok6074 <seok6074@gmail.com>
2026-09-02 14:13:40 +0900

    gg

---
+++ 26031003_SukSeungMin_Csharptraing/26031003_SukSeungMin_BasicTraing/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _26031003_SukSeungMin_BasicTraing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //// 키워드, 식별자 연습.
+            //string name = "오희성";
+            //string id1000000 = "924545";
+            //string _subject = "객체지향 c#";
+
+            ////$과 {} 넣음으로써 출력 가능
+            //Console.WriteLine($"이름 : {name}\n, id: {id1000000}\n, 과목: {_subject}");
+
+            //연산 연습
+
+            //Console.WriteLine(10 + 2000);
+            //Console.WriteLine(10 + "2000");
+            //Console.WriteLine(Int32.Parse("10"));
+            //Console.WriteLine("10" + "2000");
+
+            //Console.WriteLine((12345 % 1)/ 10);
+            //Console.WriteLine((12345 % 100) / 10);
+            //Console.WriteLine((12345 % 1000) / 100);
+            //Console.WriteLine((12345 % 10000) / 1000);
+
+            // x -(x/y)* y
+            //Console.WriteLine(4 % 3);
+            //Console.WriteLine(4 % -3);
+            //Console.WriteLine(-4 % 3);
+            //Console.WriteLine(-4 % -3);
+
+            //Console.WriteLine(12345.0 % 1000.0);
+            //console.writeline("안녕하세요"{0});
+
+
+            //boolean
+
+            //console.WriteLine(true);
+            //console.WriteLine(false);
+
+
+            //변수
+
+            //int idNuber = 1235;
+            //double score = 98.5;
+            //char character = 'a';
+            //string message = "안녕하세요";
+
+
+            //idNuber = 15;
+            //gamemoney = 200;
+
+            //console.writeline(idnumber % gamemoney);
+
+            //int a = 21475554;
+            //int b =5255;
+            //console.writeline(a+b);
+            //console.writelune(uint.minvalue);
+
+
+        }
+        class Gamesense
+        {
+            ////생성
+            //Create();
+            ////초기화
+            //init();
+            ////갱신
+            //Update();
+            ////렌더링
+            //Render();
+            ////파괴
+            //destory();
+
+        }
+    }
+
+}   
+    
